e395e7ed940ac742a32d377a5eb1b71fa0879468
zhouzhaoxu <zhouzhaoxu@yahoo.com>
2016-06-02 22:22:35 -0700

    Update SonarSensor.java
    last version

--- SonarSensor.java
+++ SonarSensor.java
@@ -1,41 +1,65 @@
 import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.AbstractFilter;
 
+/**
+ * This is an object for the bumper sensor. It will get the data from the sensors and returns whether 
+ * it had been touched. 
+ */
 public class SonarSensor extends AbstractFilter {
+	
+	/*
+	 * The max distance the sonar should sense an object.
+	 */
 	public static final float MAX_DISTANCE = 0.9133f; // 3ft]
+	
+	/*
+	 * The minimal distance for the sonar to sense an object. 
+	 */
 	public static final float MIN_DISTANCE =  0.08f;
-//	private static final float ALPHA = 0.5f;
-//	private static final int SAMPLE_SIZE = 10;
+	
+	/*
+	 * This is used for saving an array of output from the sensor.
+	 */
 	private float[] sample;
-//	private float rollingAverage;
 
+	/**
+	 * This is the constructor to initialize the Sonar Sensor.
+	 * @param the port the sensor is using
+	 */
 	public SonarSensor(SampleProvider source) {
 		super(source);
 		sample = new float[sampleSize()];
-
-		// Initiate the rolling average;
-//		rollingAverage = 0f;
-//		for (int i = 0; i < SAMPLE_SIZE; i++) {
-//			super.fetchSample(sample, 0);
-//			rollingAverage += sample[0];
-//		}
-//		rollingAverage /= SAMPLE_SIZE;
 	}
 	
+	/**
+	 * Reading the data from the sonar sensor and return the first data.
+	 * @return return the first data from the sensor
+	 */
 	public float read() {
 		super.fetchSample(sample, 0);
-		//rollingAverage = sample[0] + ALPHA * (rollingAverage - sample[0]);
 		return sample[0];
 	}
 	
+	/**
+	 * Getting the speed ratio from the distance of the sonar to the max distance
+	 * @return return the ratio between the sonar distance to max distance
+	 */
 	public float getSpeedRatio() {
 		return read() / MAX_DISTANCE;
 	}
 	
+	/**
+	 * Checking if an object is found within the max distance
+	 * @return if an object is found within the max distance
+	 */
 	public boolean objectFound() {
 		return read() <= MAX_DISTANCE;
 	}
 	
+	/**
+	 * Checking if the the robot had reach the object.
+	 * @return if the the robot had reach the object
+	 */
 	public boolean isInvestigatComplete() {
 		return read() <= MIN_DISTANCE;
 	}
